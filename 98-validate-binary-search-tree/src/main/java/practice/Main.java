7d5668a4961c4b70e2683e1d33d9b7f950e99c8a
caiwj <caiwj@chinamobilesz.com>
2021-02-22 17:34:51 +0800

    98. 验证二叉搜索树-进行中

---
+++ 98-validate-binary-search-tree/src/main/java/practice/Main.java
@@ -0,0 +1,14 @@
+package practice;
+
+/**
+ * @author : Vander
+ * @date :   2021/2/22
+ * @description :
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 98-validate-binary-search-tree/src/main/java/practice/solution/Solution1.java
@@ -0,0 +1,16 @@
+package practice.solution;
+
+import practice.TreeNode;
+
+/**
+ * @author : Vander
+ * @date :   2021/2/22
+ * @description :
+ */
+public class Solution1 {
+
+    public boolean isValidBST(TreeNode root) {
+        return false;
+    }
+
+}
